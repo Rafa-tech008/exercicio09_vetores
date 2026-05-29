ff0cd9a4dcfb9f978d392721c0ba0a7238542735
Rafa_Ramos <rafaelramos7acsc@gmail.com>
2026-05-28 22:11:11 -0300

    Update
    Refactor functions to calculate average and count students above average. Update random number generation for scores.

--- ex04_vetores.py
+++ ex04_vetores.py
@@ -4,6 +4,7 @@
 #Início
 
 import random
+
 def func_media (vetor):
     soma= 0
     med: float = 0
@@ -11,32 +12,34 @@ def func_media (vetor):
         soma += i
     med= (soma / len (vetor))
     print (f"A média das notas do grupo, equivale a: {med: .2f}")
+    return med
 
 
-def func_calcm (vetor, media):
+def func_acima (vetor):
+    lowm = []
+    cont: int = 0
+    med= func_media (vetor)
     for i in range (len(vetor)):
-        if (vetor [i] >= media):
-            print (f"O aluno {i+1} atingiu a média: {vetor [i]}")
+        if (vetor [i] >= med):
+            cont += 1
         else:
-            print (f"O aluno {i+1} não atingiu a média: {vetor [i]}")
-
+            lowm.append (i+1)
+    print ("A quantidade de alunos acima foram:", cont)
+    print ("As posições dos alunos abaixo", lowm)
+        
+        
 
 def main ():
-    vet = []
-    media = 6.0
+    notas = []
     tamanho = 30   
     num: int = 0
 
     for i in range (tamanho):
-        num = (random.randint (0, 10))
-        vet.append (num)
+        num = (random.randint (0, 10000))
+        num = num / 1000
+        notas.append (num)
     
-    
-
-    func_calcm (vet, media)
-
-    func_media (vet)
-
+    func_acima (notas)
 
 if (__name__ == "__main__"):
     main ()
